c0bbd3cb9c2874d3aeed07310602e90159d3e9b7
ifrah1 <arifrah125@gmail.com>
2021-05-01 12:35:22 -0400

    added basic route to go home test

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+
+# Define the home view
+def home(request):
+    return HttpResponse('<h1>Hello Test</h1>')
