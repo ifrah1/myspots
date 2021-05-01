6a96285b89a0a6885ecf26a35f41cd61e3d6e399
ifrah1 <arifrah125@gmail.com>
2021-05-01 12:57:16 -0400

    added home,about and connected style sheet

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
-from django.shortcuts import HttpResponse
 
 # Create your views here.
 
 # Define the home view
 def home(request):
-    return HttpResponse('<h1>Hello Test</h1>')
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
