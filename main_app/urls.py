ec104971a3b9969c9d79e772b25e85a94363511d
ifrah1 <arifrah125@gmail.com>
2021-05-01 16:16:53 -0400

    added notes

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+
+    # route for myspots
+    # app.com/myspots shows all users spots
+    # app.com/myspots/create/ lets user create a spot to save
+    
 ]
