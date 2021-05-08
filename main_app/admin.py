d5bca70a4cdb17e769bdf5e82202a146797a3cfa
ifrah1 <arifrah125@gmail.com>
2021-05-08 18:13:40 -0400

    finished upload function

--- main_app/admin.py
+++ main_app/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 
 # import models here
-from .models import Spot
+from .models import Spot, Photo
 
 # Register your models here.
 admin.site.register(Spot)
+admin.site.register(Photo)
